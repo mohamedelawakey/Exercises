d4b708268d5590dc27e9ff3e5d2614aaf4e26003
MohamedAlawakey <mohamedelawakey@gmail.com>
2025-06-27 17:27:17 +0300

    the file 3 with cp in c++

---
+++ c++ CP/Basic_Data_Types.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <iomanip> 
+using namespace std;
+int main(){
+    int x;
+    long long int y;
+    char c;
+    float f; 
+    double d;
+
+    cin >> x >> y >> c >> f >> d;
+    cout << x << "\n" << y << "\n" << c << "\n" << f << "\n" << d << "\n";
+}

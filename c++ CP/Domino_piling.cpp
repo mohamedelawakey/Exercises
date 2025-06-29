82885f8db760da883371e3be98d48c5d760d8016
MohamedAlawakey <mohamedelawakey@gmail.com>
2025-06-29 15:08:33 +0300

    the file 12 with cp in c++

---
+++ c++ CP/Domino_piling.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <iomanip> 
+using namespace std;
+int main(){
+    int m, n, result;
+    cin >> m >> n;
+
+    result = (m * n) / 2;
+    cout << int(result) << "\n";
+}

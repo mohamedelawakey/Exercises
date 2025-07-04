f83022da3c27b123ff08e3052a1d344280b7fa68
MohamedAlawakey <mohamedelawakey@gmail.com>
2025-07-04 14:29:32 +0300

    the file 54 with cp in c++

---
+++ c++ CP/Shaass_and_Oskols.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    
+    int birds[105];  
+    for (int i = 1; i <= n; i++) {
+        cin >> birds[i];
+    }
+
+    int m;
+    cin >> m;
+
+    for (int i = 0; i < m; i++) {
+        int x, y;
+        cin >> x >> y;
+
+        if (x > 1) {
+            birds[x - 1] += y - 1;
+        }
+
+        if (x < n) {
+            birds[x + 1] += birds[x] - y;
+        }
+
+        birds[x] = 0;
+    }
+
+    for (int i = 1; i <= n; i++) {
+        cout << birds[i] << endl;
+    }
+
+    return 0;
+}

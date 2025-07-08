caccd76327f26e1c026b4102bfcd0d008c680f2a
MohamedAlawakey <mohamedelawakey@gmail.com>
2025-07-08 19:51:23 +0300

    the file 83 with cp in c++

---
+++ C++/CodeForces With C++/Long_Sequence.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n ;
+    cin >> n;
+
+    long long the_list[100001];
+    long long sum_the_list = 0;
+    for(int i = 0; i < n; i++){
+        cin >> the_list[i];
+        sum_the_list += the_list[i];
+    }
+
+    long long x;
+    cin >> x;
+
+    long long full_copies = x / sum_the_list;
+    long long current_sum = full_copies * sum_the_list;
+    long long count = full_copies * n;
+
+    for (int i = 0; i < n; ++i) {
+        current_sum += the_list[i];
+        count++;
+        if (current_sum > x) break;
+    }
+    cout << count << "\n";
+     
+}

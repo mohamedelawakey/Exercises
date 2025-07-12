101cc8b739c0197dea85111538c30fac9a1d25bb
MohamedAlawakey <mohamedelawakey@gmail.com>
2025-07-12 20:44:53 +0300

    the file 102 with cp in c++

---
+++ C++/c++ CP/AC_or_WA.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int N, M;
+    cin >> N >> M;
+
+    if (M == N) {
+        cout << "Yes" << endl;
+    } else {
+        cout << "No" << endl;
+    }
+
+    return 0;
+}
